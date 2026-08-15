fe3bc935ad8cdceda61768baf376091f0ecac693
Your Name <you@example.com>
2026-08-15 10:42:04 +0530

    Class and Object Practice

---
+++ Revision/src/basic/Student.java
@@ -0,0 +1,31 @@
+package basic;
+
+public class Student {
+  //Variables with Access Specifiers.
+	private int roll;
+	private String name;
+	private float marks;
+	
+	//Parameterized constructor
+	public Student (int roll, String name, float marks) {
+		this.roll=roll;
+		this.name=name;
+		this.marks=marks;
+	}
+	
+	public void Display() {
+		System.out.println("Roll No : "+roll);
+		System.out.println("Name : "+name);
+		System.out.println("Marks : "+marks);
+	}
+	
+	
+	public static void main(String[] args) {
+		Student s = new Student(10,"Raj",87);
+		
+		s.Display();
+		
+	}
+	
+	
+}
