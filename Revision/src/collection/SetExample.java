f4d5ff3cb702b10683b43c9050e70b637392250c
Hemraj Patil <hemrajpatil5807@gmail.com>
2026-08-17 21:21:12 +0530

    Practice of Set HashSet, LinkedHashSet

--- Revision/src/collection/SetExample.java
+++ Revision/src/collection/SetExample.java
@@ -7,21 +7,20 @@ import java.util.LinkedHashSet;
 public class SetExample {
 	
 	public static void main(String[] args) {
-		
+		// Preserv Insertion order and allow Duplicate
 		ArrayList al = new ArrayList();
 				  al.add(10);
 				  al.add(20);
 				  al.add(30);
 				  al.add(25);
 				  al.add(15);
-				  al.add(10);
-				  
+				  al.add(10);		  
 		System.out.println(al);
-		
+		// Not maintain insertion order
 		HashSet h = new HashSet(al);
 		
 		System.out.println(h);
-		
+		// Maintain insertion order and remove duplicate
 		LinkedHashSet lh = new LinkedHashSet(al);
 		
 		System.out.println(lh);
